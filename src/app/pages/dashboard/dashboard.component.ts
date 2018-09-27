9a08deb2ea5bf57d883465be7b79d338a6c6ec96
soudergt <gsouders17@gmail.com>
2018-09-27 00:07:49 -0400

    Integrated SASS

--- src/app/pages/dashboard/dashboard.component.ts
+++ src/app/pages/dashboard/dashboard.component.ts
@@ -4,18 +4,17 @@ import { DashboardService } from '../../providers/dashboard/dashboard.service';
 @Component({
   selector: 'app-dashboard',
   templateUrl: './dashboard.component.html',
-  styleUrls: ['./dashboard.component.css']
+  styleUrls: ['./dashboard.component.scss']
 })
 export class DashboardComponent implements OnInit {
 
-  data: string = '';
+  data = '';
 
   constructor(private dashboardService: DashboardService) {}
 
   ngOnInit() {
     try {
       this.data = this.dashboardService.getDash();
-      
     } catch (err) {
       console.log(err);
     }
