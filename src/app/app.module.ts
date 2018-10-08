8ec8963dc0122fe6ede382c98d7fa0e2af762d9f
soudergt <gsouders17@gmail.com>
2018-10-08 18:22:52 -0400

    Moved topbar into a component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,18 +6,22 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { FlexLayoutModule } from '@angular/flex-layout';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatCardModule } from '@angular/material/card';
+import { MatMenuModule } from '@angular/material/menu';
+import { MatIconModule } from '@angular/material/icon';
 import { AppRoutingModule } from './app-routing.module';
 import { HttpClientModule } from '@angular/common/http';
 import { DashboardComponent } from './pages/dashboard/dashboard.component';
 import { AboutComponent } from './pages/about/about.component';
 import { ContactComponent } from './pages/contact/contact.component';
+import { TopbarComponent } from './components/topbar/topbar.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     DashboardComponent,
     AboutComponent,
-    ContactComponent
+    ContactComponent,
+    TopbarComponent
   ],
   imports: [
     AppRoutingModule,
@@ -26,6 +30,8 @@ import { ContactComponent } from './pages/contact/contact.component';
     FlexLayoutModule,
     MatToolbarModule,
     MatCardModule,
+    MatMenuModule,
+    MatIconModule,
     HttpClientModule
   ],
   providers: [],
