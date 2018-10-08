0e356a81ef8ef7d1801a663d895038b3e14d90be
soudergt <gsouders17@gmail.com>
2018-10-08 19:09:38 -0400

    Login dialog component

---
+++ src/app/components/logindialog/logindialog.component.ts
@@ -0,0 +1,21 @@
+import { Component, Inject } from '@angular/core';
+import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material';
+
+export interface DialogData {
+  name: string;
+}
+
+@Component({
+  selector: 'app-logindialog',
+  templateUrl: './logindialog.component.html',
+  styleUrls: ['./logindialog.component.scss']
+})
+export class LogindialogComponent {
+
+  constructor(public dialogRef: MatDialogRef<LogindialogComponent>, @Inject(MAT_DIALOG_DATA) public data: DialogData) { }
+
+  onNoClick(): void {
+    this.dialogRef.close();
+  }
+
+}
