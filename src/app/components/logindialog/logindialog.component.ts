d2772a87c20507ef82d6c7615e3091606b40f6d3
soudergt <gsouders17@gmail.com>
2018-10-15 16:56:13 -0400

    login form and service injected

--- src/app/components/logindialog/logindialog.component.ts
+++ src/app/components/logindialog/logindialog.component.ts
@@ -1,5 +1,8 @@
+import { LoginService } from './../../providers/login/login.service';
 import { Component, Inject } from '@angular/core';
 import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material';
+import { BehaviorSubject } from 'rxjs';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 export interface DialogData {
   username: string;
@@ -13,10 +16,36 @@ export interface DialogData {
 })
 export class LogindialogComponent {
 
-  constructor(public dialogRef: MatDialogRef<LogindialogComponent>, @Inject(MAT_DIALOG_DATA) public data: DialogData) { }
+  public loginForm: FormGroup;
+
+  constructor(
+    public dialogRef: MatDialogRef<LogindialogComponent>,
+    private loginService: LoginService,
+    private formBuilder: FormBuilder,
+    @Inject(MAT_DIALOG_DATA) public data: DialogData
+  ) {
+    this.loginForm = this.formBuilder.group({
+      username: ['', Validators.required],
+      password: ['', Validators.required]
+    });
+  }
 
   onNoClick(): void {
     this.dialogRef.close();
+  }
+
+  public async login() {
+    this.loginService.login(
+      this.loginForm.controls.username.value,
+      this.loginForm.controls.password.value
+    ).subscribe({
+      error: () => {
+        console.log('error');
+      },
+      next: () => {
+        console.log('logged in');
+      }
+    });
   }
 
 }
