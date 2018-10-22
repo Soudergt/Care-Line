934c6c249266df4fc45f2f8b8c11a73066a2387a
soudergt <gsouders17@gmail.com>
2018-10-22 19:14:07 -0400

    Fixed route urls

--- server/routes/Patient/index.ts
+++ server/routes/Patient/index.ts
@@ -29,7 +29,7 @@ export class PatientRoute {
           }
         }
       },
-      url: '/course/offering'
+      url: '/getPatients'
     });
   }
 
--- server/routes/User/index.ts
+++ server/routes/User/index.ts
@@ -29,7 +29,7 @@ export class UserRoute {
           }
         }
       },
-      url: '/course/offering'
+      url: '/getUser'
     });
   }
 
