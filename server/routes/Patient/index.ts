43b7cfc686b8cd1f77356dee13f655dd3c4ca285
soudergt <gsouders17@gmail.com>
2018-10-22 17:54:12 -0400

    Patient route

---
+++ server/routes/Patient/index.ts
@@ -0,0 +1,59 @@
+import { FastifyInstance, FastifyReply, FastifyRequest } from 'fastify';
+import { IncomingMessage, ServerResponse } from 'http';
+import { PatientService } from '../../libs/PatientService';
+
+export class PatientRoute {
+  constructor(fastify: FastifyInstance) {
+    fastify.route({
+      handler: this.getPatients,
+      method: 'GET',
+      schema: {
+        querystring: {
+          properties: {
+            uid: {
+              description: 'User ID',
+              type: 'number'
+            }
+          },
+          required: ['uid'],
+          type: 'object'
+        },
+        response: {
+          400: {
+            properties: {
+              data: { type: 'object' },
+              message: { type: 'string' },
+              statusCode: { type: 'integer' }
+            },
+            type: 'object'
+          }
+        }
+      },
+      url: '/course/offering'
+    });
+  }
+
+  private async getPatients(
+    request: FastifyRequest<IncomingMessage>,
+    reply: FastifyReply<ServerResponse>
+  ) {
+    try {
+      const { uid } = request.query;
+
+      const patients = await new PatientService().getPatients(uid);
+
+      reply.code(200).send({
+        data: { patients },
+        message: 'SUCCESS',
+        statusCode: 200
+      });
+    } catch (error) {
+      reply.code(400).send({
+        data: {},
+        message: 'ERROR',
+        statusCode: 400
+      });
+    }
+  }
+}
+
---
+++ server/routes/index.ts
@@ -0,0 +1,5 @@
+import { PatientRoute } from './Patient';
+
+export default [
+  PatientRoute
+];
