d6f058a37f8cd5e98ed8c510cc22da68453b26b5
soudergt <gsouders17@gmail.com>
2018-10-23 09:22:18 -0400

    Added getUser function

--- src/app/providers/user/user.service.ts
+++ src/app/providers/user/user.service.ts
@@ -1,9 +1,22 @@
+import { environment } from './../../../environments/environment';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { HttpClient } from '@angular/common/http';
+import { map } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
 })
 export class UserService {
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  getUser(uid: string): Observable<any> {
+    return this.http.get(
+      `${environment.api}/user/getUser/${uid}`,
+      { withCredentials: true }
+    ).pipe(map((body: { data: { user: any } }) => {
+      return body.data.user;
+    }));
+  }
 }
