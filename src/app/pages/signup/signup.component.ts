b70ea616c4fc2cf6a3b6c7ece87e99ece2888302
soudergt <gsouders17@gmail.com>
2018-11-13 14:02:57 -0500

    signup page

--- src/app/pages/signup/signup.component.ts
+++ src/app/pages/signup/signup.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { LoginService } from './../../providers/login/login.service';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { BehaviorSubject } from 'rxjs';
 
 @Component({
   selector: 'app-signup',
@@ -6,10 +9,50 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./signup.component.scss']
 })
 export class SignupComponent implements OnInit {
+  public signupForm: FormGroup;
+  public STATUSES: any = {
+    UNTOUCHED: 0,
+    SIGNING_UP: 1,
+    BAD_FORM: 2,
+    BAD_REQUEST: 3
+  };
+  public status: BehaviorSubject<number>;
 
-  constructor() { }
+  constructor(
+    private formBuilder: FormBuilder,
+    private loginService: LoginService,
+    ) {
+    this.signupForm = this.formBuilder.group({
+      fn: ['', Validators.required],
+      ln: ['', Validators.required],
+      email: ['', Validators.required],
+      password: ['', Validators.required],
+      pname: ['', Validators.required]
+    });
+    this.status = new BehaviorSubject<number>(this.STATUSES.UNTOUCHED);
+   }
 
   ngOnInit() {
+  }
+
+  public async signUp() {
+    if (this.signupForm.invalid) {
+      return this.status.next(this.STATUSES.BAD_FORM);
+    }
+
+    this.status.next(this.STATUSES.SIGNING_UP);
+
+    // this.loginService.signup(
+    //   this.signupForm.controls.username.value,
+    //   this.signupForm.controls.password.value
+    // ).subscribe({
+    //   error: () => {
+    //     console.log('error');
+    //   },
+    //   next: () => {
+        
+    //   }
+    // });
   }
 
 }
