3fb77303adc30a5a353f5d50ff886dd240b017bc
soudergt <gsouders17@gmail.com>
2018-11-12 19:35:32 -0500

    patient img path changes

--- src/app/components/card-list/card-list.component.ts
+++ src/app/components/card-list/card-list.component.ts
@@ -3,14 +3,14 @@ import { Component, OnInit } from '@angular/core';
 import { Patient } from 'src/app/classes/patient';
 
 const PATIENTS: Patient[] = [
-  { id: 1, name: 'Cathy', img: 'url(/src/assets/images/people/cathy.jpg)'},
-  { id: 2, name: 'Bobby', img: 'url(/src/assets/images/people/bobby.jpg)' },
-  { id: 3, name: 'Tammy', img: 'url(/src/assets/images/people/brenda.jpg)'},
-  { id: 4, name: 'Frank', img: 'url(/src/assets/images/people/frank.jpg)' },
+  { id: 1, name: 'Cathy', img: 'url(/src/assets/images/people/patients/cathy.jpg)'},
+  { id: 2, name: 'Bobby', img: 'url(/src/assets/images/people/patients/bobby.jpg)' },
+  { id: 3, name: 'Tammy', img: 'url(/src/assets/images/people/patients/brenda.jpg)'},
+  { id: 4, name: 'Frank', img: 'url(/src/assets/images/people/patients/frank.jpg)' },
   // { id: 5, name: 'Bethany' },
-  { id: 6, name: 'George', img: 'url(/src/assets/images/people/george.jpg)'},
-  { id: 7, name: 'Patrick', img: 'url(/src/assets/images/people/patrick.jpg)'},
-  { id: 8, name: 'Duke', img: 'url(/src/assets/images/people/duke.jpg)'},
+  { id: 6, name: 'George', img: 'url(/src/assets/images/people/patients/george.jpg)'},
+  { id: 7, name: 'Patrick', img: 'url(/src/assets/images/people/patients/patrick.jpg)'},
+  { id: 8, name: 'Duke', img: 'url(/src/assets/images/people/patients/duke.jpg)'},
 ];
 
 @Component({
