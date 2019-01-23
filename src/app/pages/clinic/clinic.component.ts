e5f955df2f42a56554e1e0e449d6c109bb0cc266
soudergt <gsouders17@gmail.com>
2019-01-22 22:04:22 -0500

    clinic add user

--- src/app/pages/clinic/clinic.component.ts
+++ src/app/pages/clinic/clinic.component.ts
@@ -2,12 +2,18 @@ import { Clinic } from './../../classes/clinic';
 import { Component, OnInit } from '@angular/core';
 import { MatDialog } from '@angular/material/dialog';
 import { AddUserDialogComponent } from './../../components/add-user-dialog/add-user-dialog.component';
+import { UserService } from 'src/app/providers/user/user.service';
+import { User } from 'src/app/classes/user';
 
 const CLINIC: Clinic = {
   id: 1,
   name: 'Careline Clinic',
   address: '123 Clinic Way, Cincinnati OH 45219',
   img: "url('/assets/images/people/default.png')"
+}
+
+export interface data {
+  user: any;
 }
 
 @Component({
@@ -19,8 +25,13 @@ export class ClinicComponent implements OnInit {
   clinic: Clinic;
   lat: number;
   lng: number;
+  newUser: any;
+  users: User[];
 
-  constructor(public dialog: MatDialog) { }
+  constructor(
+    public dialog: MatDialog,
+    private userService: UserService
+  ) { }
 
   ngOnInit() {
     this.lat = 51.673858;
@@ -38,6 +49,12 @@ export class ClinicComponent implements OnInit {
 
     dialogRef.afterClosed().subscribe(result => {
       console.log('The dialog was closed');
+      this.userService.addUser(result)
+        .subscribe(user => {
+          console.log(user);
+          
+          this.users.push(user)
+        });
     });
   }
 
