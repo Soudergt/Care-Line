457abfcdad84c8ed2f5f661978ac42221354a589
soudergt <gsouders17@gmail.com>
2019-01-28 17:24:27 -0500

    fixed caretaker class

--- src/app/components/card-list/card-list.component.ts
+++ src/app/components/card-list/card-list.component.ts
@@ -126,33 +126,33 @@ const PATIENTS: Patient[] = [
   }
 ];
 const CARETAKERS: Caretaker[] = [{
-    id: 1,
     fn: 'Howard',
     ln: '',
+    clinic: 'Careline Clinic',
     img: 'url(/assets/images/people/default.png)'
   },
   {
-    id: 2,
     fn: 'Franny',
     ln: '',
+    clinic: 'Careline Clinic',
     img: 'url(/assets/images/people/default.png)'
   },
   {
-    id: 3,
     fn: 'Monica',
     ln: '',
+    clinic: 'Careline Clinic',
     img: 'url(/assets/images/people/default.png)'
   },
   {
-    id: 4,
     fn: 'Taylor',
     ln: '',
+    clinic: 'Careline Clinic',
     img: 'url(/assets/images/people/default.png)'
   },
   {
-    id: 5,
     fn: 'Madison',
     ln: '',
+    clinic: 'Careline Clinic',
     img: 'url(/assets/images/people/default.png)'
   }
 ];
