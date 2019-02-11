9401fcacdf922abc8028e58e5a95c2263355e5c2
gsouders17 <gsouders17@gmail.com>
2019-02-11 18:25:39 -0500

    added schedule service

---
+++ src/app/providers/schedule/schedule.service.ts
@@ -0,0 +1,22 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ScheduleService {
+
+  constructor(private http: HttpClient) { }
+
+  getWeekSchedule() {
+
+  }
+
+  getToday() {
+    
+  }
+
+  getAppointments() {
+
+  }
+}
