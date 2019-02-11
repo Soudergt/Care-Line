5bb1fbd6edc2536b5a4fd6dc8b30447aae1a4347
gsouders17 <gsouders17@gmail.com>
2019-02-11 18:25:30 -0500

    added noti service

---
+++ src/app/providers/notification/notification.service.ts
@@ -0,0 +1,18 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class NotificationService {
+
+  constructor(private http: HttpClient) { }
+
+  getNotifications() {
+
+  }
+
+  getUpcommingPatients() {
+    
+  }
+}
