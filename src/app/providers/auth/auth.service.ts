540b120ce6b25c18ce9d74f21da4e803aa8d3e38
gsouders17 <gsouders17@gmail.com>
2019-02-25 13:49:07 -0500

    moved to auth service

--- src/app/providers/auth/auth.service.ts
+++ src/app/providers/auth/auth.service.ts
@@ -1,11 +1,18 @@
 import { Injectable } from '@angular/core';
+import { BehaviorSubject, Observable } from 'rxjs';
+import { HttpClient } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
+  public user: BehaviorSubject<any> = new BehaviorSubject<any>(null);
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  login(username: String, password: String): Observable<any> {
+    return this.user;
+  }
 
   logout(): void {
     localStorage.setItem('isLoggedIn', "false");
