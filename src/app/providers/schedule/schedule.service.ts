709c2b8796bc1a6395ae579d5f8e31437cf5f199
gsouders17 <gsouders17@gmail.com>
2019-02-24 21:06:51 -0500

    schedule service updates

--- src/app/providers/schedule/schedule.service.ts
+++ src/app/providers/schedule/schedule.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { environment } from './../../../environments/environment';
+import { Observable } from 'rxjs';
 import { ScheduleEvent } from 'src/app/classes/scheduleEvent';
 
 @Injectable({
@@ -14,15 +15,21 @@ export class ScheduleService {
 
   }
 
-  getToday() {
-    
-  }
-
-  getAppointments() {
-
+  getEvents(selectedDate: Date): Observable<any> {
+    return this.http.get(
+      `${environment.api}/backend/schedule/getEvents/?selectedDate=${selectedDate}`
+    ); 
   }
 
   addEvent(scheduleEvent: ScheduleEvent) {
-    return this.http.post<ScheduleEvent>(`${environment.api}/backend/schedule/addEvent/`, scheduleEvent)
+    return this.http.post<ScheduleEvent>(`${environment.api}/backend/schedule/addEvent/`, scheduleEvent);
+  }
+
+  editEvent(scheduleEvent: ScheduleEvent) {
+    return this.http.post<ScheduleEvent>(`${environment.api}/backend/schedule/editEvent/`, scheduleEvent);
+  }
+
+  deleteEvent(eventID: number) {
+    return this.http.post(`${environment.api}/backend/schedule/deleteEvent/`, eventID);
   }
 }
