6ebdf0c200c3855f8da9d71abca7ce6b22768c64
gsouders17 <gsouders17@gmail.com>
2019-03-03 17:00:48 -0500

    card list updates

--- src/app/components/card-list/card-list.component.ts
+++ src/app/components/card-list/card-list.component.ts
@@ -4,6 +4,9 @@ import { Patient } from 'src/app/classes/patient';
 import { Caretaker } from './../../classes/caretaker';
 import { PatientService } from 'src/app/providers/patient/patient.service';
 import { CaretakerService } from 'src/app/providers/caretaker/caretaker.service';
+import { UserService } from 'src/app/providers/user/user.service';
+import { MatDialog } from '@angular/material/dialog';
+import { AddUserDialogComponent } from './../../components/add-user-dialog/add-user-dialog.component';
 
 @Component({
   selector: 'app-card-list',
@@ -12,12 +15,14 @@ import { CaretakerService } from 'src/app/providers/caretaker/caretaker.service'
 })
 export class CardListComponent implements OnInit {
   @Input('type') type: string;
+  @Input('admin') admin: boolean;
   activeList: any[];
   patients: Patient[];
   caretakers: Caretaker[];
 
   constructor(
     private router: Router,
+    public dialog: MatDialog,
     private patientService: PatientService,
     private caretakerService: CaretakerService
   ) { }
@@ -31,13 +36,13 @@ export class CardListComponent implements OnInit {
   }
 
   getPatients(uid: string): void {
-    this.patientService.getPatients('1').subscribe(patients => {
+    this.patientService.getPatients(uid).subscribe(patients => {
       this.activeList = patients;
     });
   }
 
   getCaretakers(uid: string): void {
-    this.caretakerService.getCaretakers('1').subscribe(caretakers => {
+    this.caretakerService.getCaretakers(uid).subscribe(caretakers => {
       this.activeList = caretakers;
     });
   }
@@ -48,6 +53,27 @@ export class CardListComponent implements OnInit {
 
   goToCaretaker(caretaker: Caretaker) {
     this.router.navigateByUrl(`/caretaker/${caretaker.id}`);
+  }
+
+  addUser(userType: string): void {
+    const dialogRef = this.dialog.open(AddUserDialogComponent, {
+      width: '600px',
+      data: {
+        type: userType
+      }
+    });
+
+    dialogRef.afterClosed().subscribe(result => {
+      if (userType === 'patient') {
+        this.patientService.addPatient(result).subscribe(newPatient => {
+          this.patients.push(newPatient);
+        });
+      } else {
+        this.caretakerService.addCaretaker(result).subscribe(newCaretaker => {
+          this.caretakers.push(newCaretaker);
+        });
+      }       
+    });
   }
 
   editPatient(selectedPatient: Patient, index: number) {
