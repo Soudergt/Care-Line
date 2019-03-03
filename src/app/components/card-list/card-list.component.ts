c638653b40f0480c38856adbde1180f3863edfbf
gsouders17 <gsouders17@gmail.com>
2019-03-03 16:28:43 -0500

    getting data from backend

--- src/app/components/card-list/card-list.component.ts
+++ src/app/components/card-list/card-list.component.ts
@@ -2,168 +2,8 @@ import { Router } from '@angular/router';
 import { Component, OnInit, Input } from '@angular/core';
 import { Patient } from 'src/app/classes/patient';
 import { Caretaker } from './../../classes/caretaker';
-import { User } from 'src/app/classes/user';
-import { UserService } from 'src/app/providers/user/user.service';
 import { PatientService } from 'src/app/providers/patient/patient.service';
-
-const PATIENTS: Patient[] = [
-  {
-    id: 1,
-    fn: 'Cathy',
-    mi: '',
-    ln: '',
-    clinic: 'Careline Clinic',
-    bday: 'May, 5, 1942',
-    gender: 'male',
-    bloodtype: 'O+',
-    height: "6'0''",
-    weight: '160 lbs',
-    img: 'url(/assets/images/people/patients/cathy.jpg)',
-    emergency: {
-      fn: 'Lauren',
-      ln: 'James',
-      address: '127 Oak Lane, Cincinnati, Ohio, 44221',
-      phone: '111-111-1111',
-      email: 'lauren.james@email.com'
-    }
-  },
-  {
-    id: 2,
-    fn: 'Bobby',
-    mi: '',
-    ln: '',
-    clinic: 'Careline Clinic',
-    bday: 'May, 5, 1942',
-    gender: 'male',
-    bloodtype: 'O+',
-    height: "6'0''",
-    weight: '160 lbs',
-    img: 'url(/assets/images/people/patients/bobby.jpg)',
-    emergency: {
-      fn: 'Lauren',
-      ln: 'James',
-      address: '127 Oak Lane, Cincinnati, Ohio, 44221',
-      phone: '111-111-1111',
-      email: 'lauren.james@email.com'
-    }
-  },
-  {
-    id: 3,
-    fn: 'Tammy',
-    mi: '',
-    ln: '',
-    clinic: 'Careline Clinic',
-    bday: 'May, 5, 1942',
-    gender: 'male',
-    bloodtype: 'O+',
-    height: "6'0''",
-    weight: '160 lbs',
-    img: 'url(/assets/images/people/patients/brenda.jpg)',
-    emergency: {
-      fn: 'Lauren',
-      ln: 'James',
-      address: '127 Oak Lane, Cincinnati, Ohio, 44221',
-      phone: '111-111-1111',
-      email: 'lauren.james@email.com'
-    }
-  },
-  {
-    id: 4,
-    fn: 'Frank',
-    mi: '',
-    ln: '',
-    clinic: 'Careline Clinic',
-    bday: 'May, 5, 1942',
-    gender: 'male',
-    bloodtype: 'O+',
-    height: "6'0''",
-    weight: '160 lbs',
-    img: 'url(/assets/images/people/patients/frank.jpg)',
-    emergency: {
-      fn: 'Lauren',
-      ln: 'James',
-      address: '127 Oak Lane, Cincinnati, Ohio, 44221',
-      phone: '111-111-1111',
-      email: 'lauren.james@email.com'
-    }
-  },
-  {
-    id: 5,
-    fn: 'George',
-    mi: '',
-    ln: '',
-    clinic: 'Careline Clinic',
-    bday: 'May, 5, 1942',
-    gender: 'male',
-    bloodtype: 'O+',
-    height: "6'0''",
-    weight: '160 lbs',
-    img: 'url(/assets/images/people/patients/george.jpg)',
-    emergency: {
-      fn: 'Lauren',
-      ln: 'James',
-      address: '127 Oak Lane, Cincinnati, Ohio, 44221',
-      phone: '111-111-1111',
-      email: 'lauren.james@email.com'
-    }
-  },
-  {
-    id: 6,
-    fn: 'Duke',
-    mi: 'M',
-    ln: 'James',
-    clinic: 'Careline Clinic',
-    bday: 'May, 5, 1942',
-    gender: 'male',
-    bloodtype: 'O+',
-    height: "6'0''",
-    weight: '160 lbs',
-    img: 'url(/assets/images/people/patients/duke.jpg)',
-    emergency: {
-      fn: 'Lauren',
-      ln: 'James',
-      address: '127 Oak Lane, Cincinnati, Ohio, 44221',
-      phone: '111-111-1111',
-      email: 'lauren.james@email.com'
-    }
-  }
-];
-const CARETAKERS: Caretaker[] = [{
-    id: 1,
-    fn: 'Howard',
-    ln: '',
-    clinic: 'Careline Clinic',
-    img: 'url(/assets/images/people/default.png)'
-  },
-  {
-    id: 2,
-    fn: 'Franny',
-    ln: '',
-    clinic: 'Careline Clinic',
-    img: 'url(/assets/images/people/default.png)'
-  },
-  {
-    id: 3,
-    fn: 'Monica',
-    ln: '',
-    clinic: 'Careline Clinic',
-    img: 'url(/assets/images/people/default.png)'
-  },
-  {
-    id: 4,
-    fn: 'Taylor',
-    ln: '',
-    clinic: 'Careline Clinic',
-    img: 'url(/assets/images/people/default.png)'
-  },
-  {
-    id: 5,
-    fn: 'Madison',
-    ln: '',
-    clinic: 'Careline Clinic',
-    img: 'url(/assets/images/people/default.png)'
-  }
-];
+import { CaretakerService } from 'src/app/providers/caretaker/caretaker.service';
 
 @Component({
   selector: 'app-card-list',
@@ -173,21 +13,33 @@ const CARETAKERS: Caretaker[] = [{
 export class CardListComponent implements OnInit {
   @Input('type') type: string;
   activeList: any[];
-  patients = PATIENTS;
-  caretakers = CARETAKERS;
+  patients: Patient[];
+  caretakers: Caretaker[];
 
   constructor(
     private router: Router,
-    private userService: UserService,
-    private patientService: PatientService
+    private patientService: PatientService,
+    private caretakerService: CaretakerService
   ) { }
 
   ngOnInit() {
     if (this.type === 'patient') {
-      this.activeList = this.patients;
+      this.getPatients('1');
     } else if (this.type === 'caretaker') {
-      this.activeList = this.caretakers;
+      this.getCaretakers('1');
     }
+  }
+
+  getPatients(uid: string): void {
+    this.patientService.getPatients('1').subscribe(patients => {
+      this.activeList = patients;
+    });
+  }
+
+  getCaretakers(uid: string): void {
+    this.caretakerService.getCaretakers('1').subscribe(caretakers => {
+      this.activeList = caretakers;
+    });
   }
 
   goToPatient(patient: Patient) {
