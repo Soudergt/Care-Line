3328a90fb60e5009603b4f9afefce0193cea6028
gsouders17 <gsouders17@gmail.com>
2019-03-03 16:28:23 -0500

    Added caretaker provider

---
+++ src/app/providers/caretaker/caretaker.service.ts
@@ -0,0 +1,21 @@
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { environment } from './../../../environments/environment';
+import { HttpClient } from '@angular/common/http';
+import { map } from 'rxjs/operators';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class CaretakerService {
+
+  constructor(private http: HttpClient) { }
+
+  getCaretakers(id: string): Observable<any> {
+    return this.http.get(
+      `${environment.api}/caretaker/getCaretakers/${id}`
+    ).pipe(map((body: {data: {caretakers: any}}) => {
+      return body.data.caretakers;
+    })); 
+  }
+}
