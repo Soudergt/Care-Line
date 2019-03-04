d96e8413f61f18a79856df5ff5331fbabb8b7571
gsouders17 <gsouders17@gmail.com>
2019-03-03 21:26:33 -0500

    caretaker updates

--- src/app/pages/caretaker/caretaker.component.ts
+++ src/app/pages/caretaker/caretaker.component.ts
@@ -1,5 +1,10 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { FormBuilder, FormGroup } from '@angular/forms';
 import { Caretaker } from 'src/app/classes/caretaker';
+import { CaretakerService } from 'src/app/providers/caretaker/caretaker.service';
+import { FeedbackService } from 'src/app/providers/feedback/feedback.service';
+import { faComment } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-caretaker',
@@ -7,17 +12,70 @@ import { Caretaker } from 'src/app/classes/caretaker';
   styleUrls: ['./caretaker.component.scss']
 })
 export class CaretakerComponent implements OnInit {
-  caretaker: Caretaker = {
-    id: 10,
-    fn: 'Taylor',
-    ln: 'Williams',
-    clinic: 'Careline Clinic',
-    img: "url('/assets/images/people/caretakers/taylorwilliams.jpg')"
-  };
+  public feedbackForm: FormGroup;
+  caretaker: Caretaker;
+  newFeedback: any;
+  faComment = faComment;
+  showNewFeedback: boolean;
+  comments: any[];
+  sub: any;
+  id: string;
 
-  constructor() { }
+  constructor(
+    private activatedRoute: ActivatedRoute,
+    private caretakerService: CaretakerService,
+    private feedbackService: FeedbackService,
+    private formBuilder: FormBuilder
+  ) {
+    this.feedbackForm = this.formBuilder.group({
+      title: [''],
+      desc: [''],
+      rating: ['']
+    });
+  }
 
   ngOnInit() {
+    this.showNewFeedback = false;
+    this.newFeedback = {};
+
+    this.sub = this.activatedRoute.params.subscribe(params => {
+      this.id = params['id'];
+      this.getCaretaker(this.id);
+    });
   }
+
+  getCaretaker(id: string): void {
+    this.caretakerService.getCaretaker(id).subscribe(caretaker => {
+      this.caretaker = caretaker
+    });
+  }
+
+  createFeedback() {
+    this.showNewFeedback = false;
+    this.newFeedback = {
+      title: this.feedbackForm.value.title,
+      desc: this.feedbackForm.value.desc,
+      rating: this.feedbackForm.value.rating
+    };
+
+    this.feedbackService.addFeedback(this.newFeedback).subscribe((comment:any) => {
+      this.comments.push(comment)
+    });
+    
+    this.feedbackForm.reset();
+  }
+
+  addFeedback() {
+    this.showNewFeedback = true;
+  }
+
+  cancelAddFeedback() {
+    this.showNewFeedback = false;
+    this.feedbackForm.reset();
+  }
+
+  ngOnDestroy() {
+    this.sub.unsubscribe();
+  };
 
 }
