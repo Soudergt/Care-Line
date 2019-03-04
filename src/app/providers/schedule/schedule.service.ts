0a2a43a7777923323a4d72629fbfcf9ef5ef8cbf
gsouders17 <gsouders17@gmail.com>
2019-03-04 18:07:28 -0500

    schedule updates

--- src/app/providers/schedule/schedule.service.ts
+++ src/app/providers/schedule/schedule.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { environment } from './../../../environments/environment';
 import { Observable } from 'rxjs';
+import { map, catchError } from 'rxjs/operators';
 import { ScheduleEvent } from 'src/app/classes/scheduleEvent';
 
 @Injectable({
@@ -11,14 +12,20 @@ export class ScheduleService {
 
   constructor(private http: HttpClient) { }
 
-  public getWeekSchedule() {
-
+  public getWeekSchedule(start: Date): Observable<any> {
+    return this.http.get(
+      `${environment.api}/schedule/getWeek/?start=${start}`
+    ).pipe(map((body: {data: {events: any}}) => {
+      return body.data.events;
+    })); 
   }
 
-  public getEvents(selectedDate: Date): Observable<ScheduleEvent> {
-    return this.http.get<ScheduleEvent>(
-      `${environment.api}/backend/schedule/getEvents/?selectedDate=${selectedDate}`
-    ); 
+  public getEvents(selectedDate: Date): Observable<any> {
+    return this.http.get(
+      `${environment.api}/schedule/getEvents/?selectedDate=${selectedDate}`
+    ).pipe(map((body: {data: {events: any}}) => {
+      return body.data.events;
+    })); 
   }
 
   public addEvent(scheduleEvent: ScheduleEvent) {
