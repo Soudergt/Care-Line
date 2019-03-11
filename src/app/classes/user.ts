d77fd552b2dbe0b936c33001b1662f51b0e46d22
gsouders17 <gsouders17@gmail.com>
2019-03-10 20:09:02 -0400

    updated User class

--- src/app/classes/user.ts
+++ src/app/classes/user.ts
@@ -1,6 +1,24 @@
 export class User {
-  fn: string;
-  ln: string;
-  username: string;
-  password: string;
+    UserID: number;
+    UserType: string;
+    UserEmail: string;
+    Password: string;
+    NamePrefix: string;
+    NameFirst: string;
+    NameMiddle: string;
+    NameLast: string;
+    NameSuffix: string;
+    JobTitle: string;
+    HomeAddress: string;
+    WorkPhone: number;
+    MobilePhone: number;
+    HomePhone: number;
+    DateOfBirth: Date;
+    FeaturesEnabled: boolean;
+    UserPhoto: string;
+    HeightInches: number;
+    WeightPounds: number;
+    BloodType: string;
+    AllergyInfo: string;
+    DietInfo: string;
 }
