450612ca95990533855ed230000e4c9a9b8ea1e5
gsouders17 <gsouders17@gmail.com>
2019-03-13 18:17:09 -0400

    message service functions

--- src/app/providers/message/message.service.ts
+++ src/app/providers/message/message.service.ts
@@ -1,20 +1,25 @@
 import { Injectable } from '@angular/core';
+import { environment } from './../../../environments/environment';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
 })
 export class MessageService {
-  messages: string[] = [];
- 
-  add(message: string) {
-    this.messages.push(message);
+
+  constructor(private http: HttpClient) { }
+
+  sendMessage(id: number, message: any): Observable<any> {
+    return this.http.post(`${environment.api}/message/send`, {id, message});
   }
 
-  getMessages() {
-
-  }
- 
-  clear() {
-    this.messages = [];
+  getMessages(id: number): Observable <any> {
+    return this.http.get(
+      `${environment.api}/message/getMessages/?id=${id}`
+    ).pipe(map((body: {data: {messages: any}}) => {
+      return body.data.messages;
+    })); 
   }
 }
