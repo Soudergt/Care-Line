849b70f96e165d31f072649b6ed85e6201074c6c
gsouders17 <gsouders17@gmail.com>
2019-03-18 19:39:39 -0400

    topbar auth check

--- src/app/components/topbar/topbar.component.ts
+++ src/app/components/topbar/topbar.component.ts
@@ -1,16 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { MatDialog } from '@angular/material/dialog';
-
+import { AuthService } from 'src/app/providers/auth/auth.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-topbar',
   templateUrl: './topbar.component.html',
   styleUrls: ['./topbar.component.scss']
 })
-export class TopbarComponent {
-
+export class TopbarComponent implements OnInit {
+  isLoggedIn: boolean;
   name: String;
 
-  constructor(public dialog: MatDialog) { }
+  constructor(
+    public dialog: MatDialog,
+    private router : Router,
+    public authService: AuthService
+  ) { }
+
+  ngOnInit() {
+    this.isLoggedIn = this.authService.verifyLogin();
+  }
+
+  logout() {
+    this.authService.logout().subscribe();
+    this.router.navigate(['/']);
+  }
 
 }
--- src/app/providers/auth/auth.service.ts
+++ src/app/providers/auth/auth.service.ts
@@ -8,24 +8,41 @@ import { map } from 'rxjs/operators';
   providedIn: 'root'
 })
 export class AuthService {
+  constructor(private http: HttpClient) { }
+
   public user: BehaviorSubject<any> = new BehaviorSubject<any>(null);
 
-  constructor(private http: HttpClient) { }
+  private loggedIn = new BehaviorSubject<boolean>(false);
+
+  get isLoggedIn() {
+    console.log(localStorage.getItem('currentUser'));
+    
+    return this.loggedIn.asObservable();
+  }
 
   public login(username: String, password: String): Observable<any> {
     return this.http.post(
       `${environment.api}/auth/login`, {username, password}
     ).pipe(map((body: {data: {user: any}}) => {
       if (body.data.user) {
-        body.data.user.authdata = window.btoa(username + ':' + password);
+        this.user.next(body.data.user);
+        this.loggedIn.next(true);
         localStorage.setItem('currentUser', JSON.stringify(body.data.user));
       }
       return body.data.user;
     })); 
   }
 
+  public verifyLogin(): boolean {
+    if (localStorage.getItem('currentUser')) {
+      return true;
+    } else {
+      return false;
+    }
+  }
+
   public logout() {
     localStorage.removeItem('currentUser');
-    return this.http.post(`${environment.api}/backend/auth/logout`, {});
-  } 
+    return this.http.post(`${environment.api}/auth/logout`, {});
+  }
 }
