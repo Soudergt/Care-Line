ac6d9b3282bec28f5e5fe3f2e18a50826feb7ed1
gsouders17 <gsouders17@gmail.com>
2019-03-18 16:23:16 -0400

    not using

--- src/app/classes/note.ts
+++
@@ -1,5 +0,0 @@
-export class Note {
-  title: string;
-  desc: string;
-  mood: string;
-}
--- src/app/classes/scheduleEvent.ts
+++
@@ -1,6 +0,0 @@
-export class ScheduleEvent {
-  name: string;
-  desc: string;
-  date: Date;
-  time: Date;
-}
