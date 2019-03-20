f0fec5abed3b7f1adb964d8af501add655597d3a
gsouders17 <gsouders17@gmail.com>
2019-03-20 19:41:03 -0400

    event services

--- src/app/providers/event/event.service.ts
+++ src/app/providers/event/event.service.ts
@@ -13,7 +13,7 @@ export class EventService {
 
   public getEvents(uid: string, date: string): Observable<any> {
     return this.http.get(
-      `/api/event/getEvents`, {params: new HttpParams().set('uid', uid).set('date', date)}
+      `/api/event/getEvents`, {params: new HttpParams().set('uid', uid).set('date', date), withCredentials: true},
     ).pipe(map((body: {data: {events: any}}) => {
       return body.data.events;
     })); 
@@ -21,7 +21,7 @@ export class EventService {
 
   public getEventsByWeek(uid: string, startDate: string): Observable<any> {
     return this.http.get(
-      `/api/event/getEventsByWeek`, {params: new HttpParams().set('uid', uid).set('startDate', startDate)}
+      `/api/event/getEventsByWeek`, {params: new HttpParams().set('uid', uid).set('startDate', startDate), withCredentials: true}
     ).pipe(map((body: {data: {events: any}}) => {
       return body.data.events;
     })); 
@@ -29,7 +29,8 @@ export class EventService {
 
   public getEvent(id: number): Observable<any> {
     return this.http.get(
-      `/api/event/getEvent/?id=${id}`
+      `/api/event/getEvent/?id=${id}`,
+      { withCredentials: true }
     ).pipe(map((body: {data: {events: any}}) => {
       return body.data.events;
     })); 
@@ -37,25 +38,28 @@ export class EventService {
 
   public addEvent(event: any): Observable<any> {    
     return this.http.post(
-      `/api/event/add`, { event }
-    ).pipe(map((body: {data: {event: any}}) => {
-      return body.data.event;
+      `/api/event/add`, { event },
+      { withCredentials: true }
+    ).pipe(map((body: {data: {newEvent: any}}) => {
+      return body.data.newEvent;
     })); 
   }
 
   public editEvent(event: any): Observable<any> {    
     return this.http.put(
-      `/api/event/edit`, { event }
-    ).pipe(map((body: {data: {event: any}}) => {
-      return body.data.event;
+      `/api/event/edit`, { event },
+      { withCredentials: true }
+    ).pipe(map((body: {data: {updatedEvent: any}}) => {
+      return body.data.updatedEvent;
     })); 
   }
 
-  public deleteUser(event: any): Observable<any> {
+  public deleteEvent(event: any): Observable<any> {
     return this.http.post(
-      `/api/event/delete`, { event }
-    ).pipe(map((body: {data: {event: any}}) => {
-      return body.data.event;
+      `/api/event/delete`, { event },
+      { withCredentials: true }
+    ).pipe(map((body: {data: {removedEvent: any}}) => {
+      return body.data.removedEvent;
     }));
   }
 }
